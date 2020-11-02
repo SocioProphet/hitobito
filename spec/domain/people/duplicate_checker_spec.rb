40e547220dacbcc4d6872a3a0f1f06f0a3544b08
Nils Rauch <rauch@puzzle.ch>
2020-11-02 09:13:29 +0100

    Expand checker specs

--- spec/domain/people/duplicate_checker_spec.rb
+++ spec/domain/people/duplicate_checker_spec.rb
@@ -6,25 +6,55 @@ describe People::DuplicateChecker do
   subject { checker.check }
 
   context 'check' do
-    it 'creates one doublette when attributes match once' do
+    it 'creates one duplicate when all attributes match once' do
       create_duplicate(:top_leader)
-      expect { subject }.to change { Person::Doublette.count }.by(1)
+      expect { subject }.to change { PersonDuplicate.count }.by(1)
     end
 
-    it 'creates two doublettes when attributes match twice' do
+    it 'creates two duplicate when all attributes match twice' do
       create_duplicate(:top_leader)
       create_duplicate(:top_leader)
-      expect { subject }.to change { Person::Doublette.count }.by(2)
+      expect { subject }.to change { PersonDuplicate.count }.by(2)
     end
 
-    it 'creates no doublette when doublet already exists' do
+    it 'creates one duplicates when names, company name, birthday match and zip code is empty' do
+      create_duplicate(:top_leader).update(zip_code: nil)
+      expect { subject }.to change { PersonDuplicate.count }.by(1)
+    end
+
+    it 'creates one duplicates when names, company name, zip code match and birthday is empty' do
+      create_duplicate(:top_leader).update(birthday: nil)
+      expect { subject }.to change { PersonDuplicate.count }.by(1)
+    end
+
+    it 'creates one duplicates when names, company name match and birthday, zip code is empty' do
+      create_duplicate(:top_leader).update(birthday: nil, zip_code: nil)
+      expect { subject }.to change { PersonDuplicate.count }.by(1)
+    end
+
+    it 'creates no duplicate when duplicate already exists' do
       duplicate = create_duplicate(:top_leader)
-      Person::Doublette.create(person_1: duplicate, person_2: people(:top_leader))
-      expect { subject }.to_not change { Person::Doublette.count }
+      PersonDuplicate.create(person_1: duplicate, person_2: people(:top_leader))
+      expect { subject }.to_not change { PersonDuplicate.count }
     end
 
-    it 'creates no doublette when no attributes match' do
-      expect { subject }.to_not change { Person::Doublette.count }
+    it 'creates no duplicate when no attributes match' do
+      expect { subject }.to_not change { PersonDuplicate.count }
+    end
+
+    it 'creates no duplicate if first_name does not match' do
+      create_duplicate(:top_leader).update(first_name: 'Bottom')
+      expect { subject }.to_not change { PersonDuplicate.count }
+    end
+
+    it 'creates no duplicate if last_name does not match' do
+      create_duplicate(:top_leader).update(first_name: 'Member')
+      expect { subject }.to_not change { PersonDuplicate.count }
+    end
+
+    it 'creates no duplicate if company_name does not match' do
+      create_duplicate(:top_leader).update(company_name: 'Company')
+      expect { subject }.to_not change { PersonDuplicate.count }
     end
   end
 
